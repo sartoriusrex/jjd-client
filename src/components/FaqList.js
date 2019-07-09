312f531b1b282c33a7ace99469e058adb70994bb
sartoriusrex <dennis.q.mai@gmail.com>
2019-07-09 11:41:03 -1000

    create new faqlist for easy update and import to faqpage to render dynamically

---
+++ src/components/FaqList.js
@@ -0,0 +1,14 @@
+export const Faqs = [
+  {
+    question: "Can I create entries and reactions when I create a technique?",
+    answer: "At the moment, that is not a possibility."
+  },
+  {
+    question: "Do I need to make an account to create and edit techniques and sequences?",
+    answer: "Yes. Anonymous users can browse what has been created, but to ensure that we can properly control the quality of the content, users must register with a working e-mail to which we can associate their techniques and sequences."
+  },
+  {
+    question: "Can I give you tips, feedback, criticism, etc?",
+    answer: "Yes, please visit the contribute page while logged in to send us an email."
+  }
+]
--- src/components/FaqPage.js
+++ src/components/FaqPage.js
@@ -1,6 +1,48 @@
 import React from 'react';
 
+import { Faqs } from "./FaqList";
+
 const FaqPage = () => {
+
+  const renderCardHeader = ( question, index, ) => {
+    return(
+      <div className="card-header p-0 bg-transparent" id={`headingOne${ index }`}>
+        <h3 className="mb-0">
+          <button className="btn pl-0 pr-0" type="button" data-toggle="collapse" data-target={`#collapse${ index }`} aria-expanded="true" aria-controls={`collapse${ index }`}>
+            - { question }
+          </button>
+        </h3>
+      </div>
+    )
+  }
+
+  const renderCardBody = ( answer, index ) => {
+    return(
+      <div id={`collapse${ index }`} className="collapse" aria-labelledby={`heading${ index }`} data-parent="#faqAccordion">
+        <div className="card-body">
+          <p>
+            { answer }
+          </p>
+        </div>
+      </div>
+    )
+  }
+
+  const renderCards = () => {
+    return(
+      Faqs.map( ( faq, i ) => {
+        return(
+          <div 
+            key={ faq.question} 
+            className="card bg-transparent border-0 border-top border-bottom">
+              { renderCardHeader( faq.question, i ) }
+              { renderCardBody( faq.answer, i ) }
+          </div>
+        )
+      })
+    )
+  }
+
   return(
     <div className="container main min-vh-100 pt-3 pl-1 pr-1">
       <div
@@ -9,42 +51,7 @@ const FaqPage = () => {
         <h2 className="pt-4 pb-4">FAQ</h2>
 
         <div className="accordion pt-2 pb-2 w-100 bg-transparent" id="faqAccordion">
-
-          <div className="card bg-transparent border-0 border-top border-bottom">
-            <div className="card-header p-0 bg-transparent" id="headingOne">
-              <h3 className="mb-0">
-                <button className="btn pl-0 pr-0" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                  - Can I create entries and reactions when I create a technique?
-                </button>
-              </h3>
-            </div>
-
-            <div id="collapseOne" className="collapse" aria-labelledby="headingOne" data-parent="#faqAccordion">
-              <div className="card-body">
-                <p>
-                  At the moment, that is not a possibility.
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="card bg-transparent border-0 border-top border-bottom">
-            <div className="card-header p-0 bg-transparent" id="headingOne">
-              <h3 className="mb-0">
-                <button className="btn pl-0 pr-0" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                  - Do I need to create an account to create, edit, and share techniques and sequences?
-                </button>
-              </h3>
-            </div>
-
-            <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#faqAccordion">
-              <div className="card-body">
-                <p>
-                  Yes, currently that is the only way assure quality-control from our users. This has the added benefit of linking techniques and sequences to your and others' accounts for later reference.
-                </p>
-              </div>
-            </div>
-          </div>
+          { renderCards() }
         </div>
       </div>
     </div>
